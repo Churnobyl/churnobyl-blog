d5b8106b0303672af9b351e03c0f7527d589fa32
Churnobyl <tjdcjfals@gmail.com>
2025-01-07 14:41:59 +0900

    add preconnect

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,9 +3,15 @@ require("prism-themes/themes/prism-coldark-dark.css");
 require("prismjs/plugins/line-numbers/prism-line-numbers.css");
 
 export const onInitialClientRender = () => {
+  const preconnect = document.createElement("link");
+  link.rel = "preconnect";
+  link.href = "https://cdn.jsdelivr.net";
+
   const link = document.createElement("link");
   link.rel = "stylesheet";
   link.href =
     "https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/variable/pretendardvariable-dynamic-subset.min.css";
+
+  document.head.appendChild(preconnect);
   document.head.appendChild(link);
 };
